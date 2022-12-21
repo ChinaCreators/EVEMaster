b0ebf8d45fa6a5e3717ea03c8bfbad0e9692903b
creatorlxd <1461637345@qq.com>
2022-12-21 21:46:21 +0800

    fix translate in cmake

--- resource/EVEMaster_zh_CN.ts
+++ resource/EVEMaster_zh_CN.ts
@@ -1,3 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../source/mainwindow.ui" line="14"/>
+        <source>EVEMaster</source>
+        <translation type="unfinished">EVE大师</translation>
+    </message>
+    <message>
+        <location filename="../source/mainwindow.ui" line="27"/>
+        <source>test</source>
+        <translation type="unfinished">测试</translation>
+    </message>
+</context>
+</TS>
